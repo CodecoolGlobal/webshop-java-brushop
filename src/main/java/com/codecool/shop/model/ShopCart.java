da92446e41d6cc88cc34fa75f4c58226a89b43ea
Kforkristof <kmintkristof@gmail.com>
2019-07-23 14:37:48 +0200

    added Cartcontroller, ShopCart java and html file

---
+++ src/main/java/com/codecool/shop/model/ShopCart.java
@@ -0,0 +1,34 @@
+package com.codecool.shop.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class ShopCart {
+    private static HashMap<Product, Integer> shoppingCart = new HashMap<>();
+
+    ShopCart(){}
+
+    void addToCart(Product product){
+        if (getShoppingCart().keySet().contains(product)){
+            getShoppingCart().put(product, numOfOrder(product) + 1);
+        } else {
+            getShoppingCart().put(product, 1);
+        }
+    }
+
+    public static HashMap<Product, Integer> getShoppingCart() {
+        return shoppingCart;
+    }
+
+
+    public Integer numOfOrder(Product product){
+        return getShoppingCart().get(product);
+    }
+
+    public void removeFromCart(Product product){
+        getShoppingCart().remove(product);
+    }
+
+}
